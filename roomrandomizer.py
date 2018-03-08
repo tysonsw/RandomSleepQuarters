ce176b6c84f1e00f0fbc84226a5b9f4a34aff923
tysonsw <35592963+tysonsw@users.noreply.github.com>
2018-03-08 15:12:11 +0100

    Cleaned the code a bit and added comments

--- roomrandomizer.py
+++ roomrandomizer.py
@@ -7,7 +7,6 @@ This program takes two input files with lists of participants and a list of room
 
 '''
 
-
 import csv 
 from random import *
 import random
@@ -15,42 +14,32 @@ import locale
 import logging
 from sys import argv
 import os
-#locale.getpreferredencoding(do_setlocale=True)
+
 clear = lambda: os.system('cls')
-#logging.basicConfig(level=logging.INFO)
 secure_random = random.SystemRandom()
 
 decided_location = []
-parti_file_input =""
-parti_file = ""
-room_file_input = ""
-room_file = ""
-parti_columns =""
-parti_needs =""
-room_columns = ""
-room_needs =""
-
 
 def read_file(rfile):
 	try:
 		f = open (rfile, 'r', encoding = 'latin')
 
 	except FileNotFoundError as e:
-		print ("Couldn't find file. Try again. ")
+		print ("Couldn't find file. Please try again. ")
 		raise
 	
 	reader = csv.reader(f, delimiter=";")
 	return reader
 
 	
-
+#This function reaturns a list of participants with needs defined by "x"
 def parti_needs_list(all_participant,parti):
 	
 	with_needs = []
 
 	for x in all_participant:
 		for i in parti:
-			if x[int(i)]== "x":
+			if x[i]== "x":
 				with_needs.append(x)
 				break
 
@@ -78,47 +67,38 @@ def find_room(parti,room, room_file_input, parti_file_input):
 	all_participant.pop(0)
 
 	
-	for x in parti_needs_list(all_participant,parti): #hitta behovs personer och loopa listan
+	for x in parti_needs_list(all_participant,parti): #find all participants that have specific needs
 		hits = []
 		for z in all_rooms:
-			for i in range(len(parti)): #loopa behovs kolumnerna
-				if x[int(parti[i])] == z[room[i]]:
-					#print ("Add 1: {}".format(z))
+			for i in range(len(parti)):
+
+				if x[parti[i]] == z[room[i]]:
 					if z in hits:
 						continue
 					hits.append(z)
 					continue
 
-				if x[int(parti[i])] in (None, "") and z[room[i]] =="x" :
-					#print ("Add 2: {}".format(z))
+				if x[parti[i]] in (None, "") and z[room[i]] =="x" :
 					if z in hits:
 						continue
 					hits.append(z)
 
-					
-				if  x[int(parti[i])] == "x" and z[room[i]] in (None, "") :
-					#print ("Remove 1: {}".format(z))
+				#breaks out and discards the chosen room since it doesn't fit the need.
+				if  x[parti[i]] == "x" and z[room[i]] in (None, "") :
 					if z in hits:
 						hits.remove(z)
 					break				
 
 		try:
 			random_room = secure_random.choice(hits)
-			
+			if random_room in all_rooms:
+				add_output(output_parti=x,output_room=random_room)
+				all_rooms.remove(random_room)
+
 		except IndexError:
 			add_output(output_parti=x,output_exception="Can't find room that meet needs")
 			continue
 		
-		try: 
-			if random_room in all_rooms:
-			
-				all_rooms.remove(random_room)
-				add_output(output_parti=x,output_room=random_room)
-
-		except IndexError:
-			add_output (output_parti=x,output_exception="Can't find room that meet needs")
-			continue
-
 		if x in all_participant:
 			
 			all_participant.remove(x)
@@ -128,20 +108,14 @@ def find_room(parti,room, room_file_input, parti_file_input):
 		
 		try: 
 			random_room = secure_random.choice(all_rooms)
-			
-		except IndexError:
-			add_output(output_parti=e,output_exception="Missing Room")
-			continue
-			
-		try:
 			if random_room in all_rooms:
-				
 				add_output(output_parti=e,output_room=random_room)
 				all_rooms.remove(random_room)
-				
+			
 		except IndexError:
 			add_output(output_parti=e,output_exception="Missing Room")
 			continue
+
 
 	#add all unused rooms to the output file
 	for i in all_rooms:
@@ -149,7 +123,9 @@ def find_room(parti,room, room_file_input, parti_file_input):
 
 
 	return decided_location
-	
+
+#Checks with rows the user ha chosen for the output fil
+#and adds these to decided_location for later write to the output file.	
 def add_output(output_parti=None,output_room=None,output_exception=None):
 	output_row = []
 	
@@ -168,6 +144,8 @@ def add_output(output_parti=None,output_room=None,output_exception=None):
 
 	decided_location.append(output_row)
 
+#Cleans the input and removes "," and 
+#the extra 1 that has been added to the column number by the user. 
 def clean_input(clean):
 	clean = clean.split(',')
 
@@ -176,7 +154,8 @@ def clean_input(clean):
 	
 	return clean
 
-def print_first(column_name):
+#Prints the headers of the csv files and add ones for simplisity for the user.
+def print_first(column_name): 
 	number = 1
 	rows = list(column_name)
 	for x in rows[0]:
@@ -184,11 +163,15 @@ def print_first(column_name):
 		number += 1
 
 def write_output(result):
-
-	with open('output_file.csv', 'w', encoding = 'latin') as csvoutput:
-		writer = csv.writer(csvoutput, lineterminator='\n', delimiter=";")
-		for place in result:
-			writer.writerows([place])
+	while True:
+		try:
+			with open('output_file.csv', 'w', encoding = 'latin') as csvoutput:
+				writer = csv.writer(csvoutput, lineterminator='\n', delimiter=";")
+				for place in result:
+					writer.writerows([place])
+				break
+		except PermissionError:
+			input("Can't create the output file. \nPlease check that it isn't open and make sure that you have permission to the location and then press Enter...")
 
 	print("")			
 	print("The sleeping locations has now been saved to output_file.csv")
@@ -244,4 +227,3 @@ room_needs = clean_input(input("Which numbers include specific needs? Seperate b
 
 result = find_room(parti_needs, room_needs,room_file_input, parti_file_input)
 write_output(result)
-
